65bd9884d4c2834eec5ecbe44807fede362b6338
Egor <89373012+EgorEngineer@users.noreply.github.com>
2024-09-21 10:05:48 +0400

    1

---
+++ Interfaces/IVotingService.cs
@@ -0,0 +1,16 @@
+﻿using Prioritezator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prioritezator.Interfaces
+{
+    public interface IVotingService
+    {
+        Task<List<Feature>> GetFeaturesAsync();
+        Task<Feature> VoteAsync(int featureId);
+    }
+
+}
---
+++ Models/Feature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+
+namespace Prioritezator.Models
+{
+    public class Feature
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Votes { get; set; } = 0;
+    }
+}
+
